67dab01713df9038e6b975306904bead6bd87b52
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2026-04-05 16:50:01 -0500

    Add: queue - submission-1

---
+++ Data Structures & Algorithms/queue/submission-1.py
@@ -0,0 +1,19 @@
+class Deque:
+
+    def __init__(self):
+        self.dq = deque()
+
+    def isEmpty(self) -> bool:
+        return len(self.dq) == 0
+
+    def append(self, value: int) -> None:
+        self.dq.append(value)
+
+    def appendleft(self, value: int) -> None:
+        self.dq.appendleft(value)
+
+    def pop(self) -> int:
+        return self.dq.pop() if self.dq else -1
+
+    def popleft(self) -> int:
+        return self.dq.popleft() if self.dq else -1
