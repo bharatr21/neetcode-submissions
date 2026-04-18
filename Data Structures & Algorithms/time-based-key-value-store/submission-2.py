38a033c52c2421e8736608f01d9c0bbeae2a7678
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2026-04-18 01:39:07 -0500

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,16 @@
+from bisect import bisect_right
+import operator
+
+class TimeMap:
+
+    def __init__(self):
+        self.tmap = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.tmap[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.tmap:
+            idx = bisect_right(self.tmap[key], timestamp, key=operator.itemgetter(0))
+            return self.tmap[key][idx - 1][1] if idx > 0 else ""
+        return ""
